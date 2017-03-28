ef2a63bd728f40e63ce8998bdbcae5da2e51b5c6
Storm <wenyejie@foxmail.com>
2017-03-28 14:08:13 +0800

    初始化路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+
+  // 开启 HTML5 History 模式
+  mode: 'history',
+
+  // 路由变更之后的滚动行为
+  scrollBehavior (to, form, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return {x: 0, y: 0}
+  },
+
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: resolve => {
+        require(['@/view'], resolve);
+      }
     }
   ]
 })
