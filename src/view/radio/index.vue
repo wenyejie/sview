83d47b8fb17913bcc416b24b2d93f8010d142b7f
Storm <wenyejie@foxmail.com>
2017-03-28 15:58:04 +0800

    create radio component example

---
+++ src/view/radio/index.vue
@@ -0,0 +1,29 @@
+<!--
+ - author: Storm
+ - date: 2017/03/28
+ -->
+
+<template>
+  <div class="radioExample">
+    <radio>DEMO</radio>
+
+    <radio-group>
+      <radio label="beijing" name="city">北京</radio>
+      <radio label="shanghai" name="city">上海</radio>
+      <radio label="guangzhou" name="city">广州</radio>
+      <radio label="hangzhou" name="city">杭州</radio>
+      <radio label="shenzhen" name="city">深圳</radio>
+    </radio-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RadioExample',
+    props: {},
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
