eb0f28b2fb5ab2240766c8c96e97929df5fa43e1
Storm <wenyejie@foxmail.com>
2017-04-10 09:27:05 +0800

    add message example

--- src/view/message/index.vue
+++ src/view/message/index.vue
@@ -8,13 +8,12 @@
     <h1>Message</h1>
 
     <section>
-      <h3 class="example-title">innerHTML - 内置</h3>
-      <s-button type="primary" @click="show001 = true">显示</s-button>
-      <s-dialog v-model="show001"></s-dialog>
+      <h3 class="example-title">success - 成功</h3>
+      <s-button type="primary" @click="show001">显示</s-button>
     </section>
 
     <section>
-      <h3 class="example-title">function - 函数</h3>
+      <h3 class="example-title">warning - 警告</h3>
       <s-button type="primary" @click="show002">显示</s-button>
     </section>
 
@@ -27,10 +26,12 @@
     props: {},
     data () {
       return {
-        show001: false
       }
     },
     methods: {
+      show001 () {
+        this.$Message.success('提交成功!');
+      },
       show002 () {
         this.$Message.warning('网络异常,稍后请重试')
           /*.then(() => {
